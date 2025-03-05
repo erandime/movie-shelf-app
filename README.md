5478cb689d93c617202a6435f72a2c46fddf19f2
erandime <epmadawalagama@gmail.com>
2025-03-06 02:24:37 +0800

    Finish basic backend development for project.

--- README.md
+++ README.md
@@ -1,8 +1,65 @@
-Video for larger screen, image for smaller screen.
-Sidebar for larger screen, top nav for smaller screens.
-header and footer for reusability.
-responsive design for all elements.
-bootstrap extensive use.
-aria labels
-promps to confirm delete
- 
+# Movie Shelf
+
+Movie Shelf is a movie rating website that allows users to view, rate, and manage a collection of movies. 
+
+## Features
+
+- **Responsive Design**: The layout adapts to both larger and smaller screens:
+  - For larger screens, a sidebar navigation is displayed.
+  - For smaller screens, the top navigation is used for better accessibility.
+  - Video is displayed for larger screens, while images are used for smaller screens.
+  - Bootstrap and Media queries to ensure responsiveness for all elements across devices
+  
+- **Star Rating**: Users can rate movies using a star rating system.
+  
+- **CRUD Operations**: Users can Create, Read, Update, and Delete movie entries from the PostgreSQL database.
+  
+- **Data Persistence**: Movie data is stored persistently in a PostgreSQL database.
+
+- **Express/Node.js**: The server-side logic is built with Express.js and Node.js.
+  
+- **EJS Templating**: Views are rendered dynamically using EJS (Embedded JavaScript templates).The header and footer are reusable components (header.ejs, footer.ejs).
+  
+- **ARIA Labels**: Accessibility is ensured with ARIA labels for screen readers.
+
+## Prerequisites
+
+Before getting started, make sure you have the following installed:
+
+- [Node.js]
+- [PostgreSQL]
+- [Git]
+
+## Installation
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/erandime/movie-shelf.git
+cd movie-shelf
+```
+
+2. Install dependencies:
+
+```bash
+npm install
+```
+
+3. Set up PostgreSQL database. The database.sql provides an example of setting up database called "movies" used for this project.
+
+4. Configure environment variables:
+Create a .env file at the root of the project.
+Add the following:
+
+DB_HOST=localhost
+DB_PORT=5432
+DB_USER=your-db-username
+DB_PASSWORD=your-db-password
+DB_NAME=movies
+
+5. Start the development server:
+
+```bash
+node index.js
+```
+6. Visit http://localhost:3000 in your browser to access the app.
--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 const port = 3000;
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "movies",
-  password: "qwerty",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 db.connect();
 
@@ -27,9 +29,9 @@ app.get("/add", async (req, res) => {
 
 app.get("/movies", async (req, res) => {
   try {
-    const result = await db.query("SELECT id, movie_name, genre, rating FROM movies;"  );
-  let movies = [];
-  result.rows.forEach((movie) => {
+    const result = await db.query("SELECT id, movie_name, genre, rating FROM movies ORDER BY id ASC;"  );
+    let movies = [];
+    result.rows.forEach((movie) => {
     movies.push({
       id: movie.id,
       movie_name: movie.movie_name,
@@ -38,10 +40,9 @@ app.get("/movies", async (req, res) => {
     });
   });
   res.render("movies.ejs", {movies});
-  console.log(result.rows);
-
   } catch (error) {
-    console.log(error);
+    console.log("Database query failed", error);
+    res.status(500).send("There was an error fetching the movies. Please try again later."); //Display error in future version
   }  
 });
 
@@ -66,10 +67,35 @@ app.get("/view/:id", async (req, res) => {
       watch_status: movie.watch_status,
       watch_date: movie.watch_date,
     });
-  console.log(result.rows);
 
   } catch (error) {
-    console.log(error);
+    console.log("Database query failed", error);
+    res.status(500).send("There was an error fetching the movie. Please try again later.");
+  }  
+});
+
+app.post("/update", async (req, res) => {
+  const id = req.body.id;
+  const movie_name = req.body["movie_name"];
+  const release_date = req.body["release_date"] || null;
+  const overview = req.body["overview"] || null;
+  const genre = req.body["genre"];
+  const upload_image = req.body["upload_image"] || null;
+  const rating = req.body["rating"] ? parseInt(req.body["rating"]) : null;
+  const review = req.body["review"] || null;
+  const watch_status = req.body["watch_status"];
+  const watch_date = req.body["watch_date"] || null;
+  
+  try {
+    const result = await db.query(
+    "UPDATE movies SET movie_name = $1, release_date = $2, overview = $3, genre = $4, upload_image = $5, rating = $6, review = $7, watch_status = $8, watch_date = $9 WHERE id = $10",
+    [movie_name, release_date, overview, genre, upload_image, rating, review, watch_status, watch_date, id] 
+  );  
+        
+    return res.render("view.ejs", { id, movie_name, release_date, overview, genre, upload_image, rating, review, watch_status, watch_date});
+  } catch (error) {
+    console.log("Database query failed", error);
+    res.status(500).send("Error in update. Please Try again later.");
   }  
 });
 
@@ -82,37 +108,21 @@ app.post("/add", async (req, res) => {
     const rating = req.body["rating"] ? parseInt(req.body["rating"]) : null;
     const review = req.body["review"] || null;
     const watch_status = req.body["watch_status"];
-  const watch_date = req.body["watch_date"] || null;
-  
-  // Handles Update
-  if (req.body.id) { 
-    try {
-      const id = req.body.id;
+    const watch_date = req.body["watch_date"] || null;  
 
-    const result = await db.query(
-    "UPDATE movies SET movie_name = $1, release_date = $2, overview = $3, genre = $4, upload_image = $5, rating = $6, review = $7, watch_status = $8, watch_date = $9 WHERE id = $10",
-    [movie_name, release_date, overview, genre, upload_image, rating, review, watch_status, watch_date, id] 
-  );  
-  console.log("Updated movie", result.rows);
-    return res.render("view.ejs", { id, movie_name, release_date, overview, genre, upload_image, rating, review, watch_status, watch_date, id});
-  } catch (error) {
-    console.log(error);
-  }  
-  };
-
-  // Handles add
   try {
   const result = await db.query(
     "INSERT INTO movies (movie_name, release_date, overview, genre, upload_image, rating, review, watch_status, watch_date) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id",
     [movie_name, release_date, overview, genre, upload_image, rating, review, watch_status, watch_date] 
   );  
-  console.log("Added new movie:", result.rows);
+    console.log("Added new movie:", result.rows[0].id);
     res.render("view.ejs", {
       id: result.rows[0].id,
       movie_name, release_date, overview, genre, upload_image, rating, review, watch_status, watch_date
     });
   } catch (error) {
-    console.log(error);
+    console.log("Database query failed", error);
+    res.status(500).send("There was an error adding the movie. Please try again later.");
   }  
 });
 
@@ -122,10 +132,11 @@ app.post("/delete", async (req, res) => {
     const result = await db.query("DELETE FROM movies WHERE id = $1;",
       [id]
     );
-    console.log(result.rows);
+    
     res.redirect("/movies");
   } catch (error) {
-    console.log(error);
+    console.log("Database query failed", error);
+    res.status(500).send("There was an error deleting the movie. Please try again later.");
   }
 });
 
